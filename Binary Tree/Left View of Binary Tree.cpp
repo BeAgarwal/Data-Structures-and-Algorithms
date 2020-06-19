f881cef15c3d80f6727c85d9acf7a30916fb4a1b
Shubham Agarwal <agarwalshubham.23@gmail.com>
2020-06-19 15:19:30 +0530

    Left View of Binary Tree
    Given a Binary Tree, print left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from left side

---
+++ Binary Tree/Left View of Binary Tree.cpp
@@ -0,0 +1,73 @@
+//Code written by Shubham Agarwal
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int info;
+    Node *left = NULL;
+    Node *right = NULL;
+    int hd;
+};
+
+Node *root = NULL;
+
+Node *newNode(int data) {
+    Node *temp = new Node;
+    temp->info = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+void inorder(Node *root) {
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->info << " ";
+    inorder(root->right);
+}
+
+void leftView(Node *node) {
+    if (node == NULL)
+        return;
+
+    queue<Node *> q;
+    q.push(node);
+    while (!q.empty()) {
+        int n = q.size();
+
+
+        for (int i = 0; i < n; i++) {
+            Node *temp = q.front();
+            q.pop();
+            if (i == 0)
+                cout << temp->info << " ";
+
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+
+}
+
+int main() {
+
+    root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->right = newNode(5);
+    root->left->right->left = newNode(6);
+    root->left->right->left->left = newNode(7);
+    root->left->right->right = newNode(8);
+    root->right->right = newNode(9);
+    inorder(root);
+    cout << endl;
+    
+    leftView(root); //1 2 5 6 7
+
+    return 0;
+}
